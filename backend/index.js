a0017c6a0d96d77c85cd6c7dcf0bf23230da324f
Ayushi0516 <ayushigupta0516@gmail.com>
2023-05-21 00:16:48 +0530

    login signup added

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,26 @@
-  const express=require("express")
-  const {userController}=require("./routes/user.routes")
-const {connection}=require("./config/db")
-  const app=express()
-  const PORT=8080;
-  app.use(express.json())
+const express = require("express");
+const {userController} = require("./routes/user.routes")
+const { connection } = require("./config/db");
+const {authentication} = require("./middlewares/authentication")
+const cors = require("cors")
+const app = express();
+const PORT = 8080;
+app.use(express.json());
 
-  app.get("/",(req,res)=>{
-    res.send("homepage")
-  })
-  app.use("/user",userController)
-  app.listen(PORT,async()=>{
-    try{
-     await connection;
-     console.log("connected to db")
-    }
-    catch(error){
-        console.log(error)
-    }
-    console.log(`Listening on PORT ${PORT}`)
-  })
+app.get("/", (req, res) => {
+  res.send("homepage");
+});
+app.use(cors())
+
+app.use("/user", userController);
+app.use(authentication)
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("connected to db");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(`Listening on PORT ${PORT}`);
+});
