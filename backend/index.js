268f92368a9843e07bdf1356e5967099ac75cc8a
Ayushi0516 <ayushigupta0516@gmail.com>
2023-05-20 17:13:31 +0530

    initial setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("intial setup")
+  const express=require("express")
+
+  const app=express()
+  const PORT=8080;
+  app.use(express.json())
+
+  app.get("/",(req,res)=>{
+    res.send("homepage")
+  })
+
+  app.listen(PORT,()=>{
+    console.log(`Listening on PORT ${PORT}`)
+  })
